e7dca05ff1e30efb8251c34455bf0ad45ce86afb
Matteojk95 <matteopalmer2095@gmail.com>
2023-08-02 13:04:33 +0200

    Manca generare l'età in modo casuale

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,5 +1,7 @@
 package org.java;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         String[] nomi = {"michele dalsecco", "paolo albini", "michele ferrante,", "simone zaccagni", "paolo iniesta",
@@ -7,6 +9,13 @@ public class Main {
         "simone bini", "fabio nocciolo", "francesco limone", "gianluigi petrarca", "luigi nzoie", "francesco scamacca",
         "michele rocca", "luigi stoduto", "jacopo ugolini", "davide santerello", "michele depedis", "luca biriolo"};
 
-        String[] ruoli={            }
+        String[] ruoli={ "difensore", "centrocampista", "attaccante"};
+
+        Random rand = new Random();
+        int numeroCasualeperNames =rand.nextInt(0, nomi.length);
+        int numeroCasualeperRuoli= rand.nextInt(0, ruoli.length);
+        System.out.println(nomi[numeroCasualeperNames]+" "+ ruoli[numeroCasualeperRuoli]);
+
+
     }
 }
